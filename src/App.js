dbd093df63173a38ac5db907e5d7f5821db2fab0
Harish Swaminathan <harishswami62@gmail.com>
2021-10-14 11:00:20 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import "./App.css";
+import "./App.scss";
 import React, { Component } from "react";
 
 class App extends Component {
   render() {
     return (
-      <div className="main">
-        <div>Nav</div>
-        <div>home</div>
-        <div>about</div>
-        <div>resume</div>
-        <div>projects</div>
-        <div>contact</div>
+      <div className="App">
+        <div id="page">
+          <div>Nav</div>
+          <div>home</div>
+          <div>about</div>
+          <div>resume</div>
+          <div>projects</div>
+          <div>contact</div>
+        </div>
       </div>
     );
   }
